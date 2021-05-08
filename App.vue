50c4a7db9cc576b06acd40ceb97aea2084e1625a
eric_uuu <cory_uuu@sina.cn>
2021-05-08 16:13:58 +0800

    提交最新demo

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
-			//plus.screen.lockOrientation('portrait-primary'); //锁定
+			plus.screen.lockOrientation('portrait-primary'); //锁定
 		},
 		onShow: function() {
 			console.log('App Show')
